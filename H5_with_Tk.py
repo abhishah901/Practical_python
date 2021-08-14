40b900ac4a62a4488559b346f6db6c59aee93a36
Abhishek Shah <ashah@ironsidegroup.com>
2021-08-14 16:44:09 -0400

    Get an h5 model to predict with GUI in py

---
+++ H5_with_Tk.py
@@ -0,0 +1,46 @@
+# IMPORTS
+from PIL import Image, ImageTk
+import numpy as np
+from tensorflow.keras.models import load_model
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+# import keras.engine
+
+# Get model
+model = load_model('complex_model.h5')
+
+
+# Prompt user to select a file
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
+print(file_path)  # GET FILE PATH
+
+
+# Preprocess image to feed the model
+img = Image.open(file_path).convert('L').resize((48, 48))
+ITK = ImageTk.PhotoImage(img)
+np_img = np.array(img)
+img_array = np_img.reshape(48, 48, 1)
+
+
+# Use model to predict
+
+# angry: 0, happy: 1, sad
+hold_pred = np.argmax(model.predict(img_array[None,:,:]))
+
+def prompt(message, img):
+    messagebox.showinfo('Predictions', message)
+    # root = Tk()
+    # panel = Label(root, image = img)
+    # panel.pack(side = "bottom", fill = "both", expand = "yes")
+    # root.mainloop()
+
+if hold_pred == 0:
+    prompt('Angry', ITK)
+elif hold_pred == 1:
+    prompt('Happy', ITK)
+elif hold_pred == 2:
+    prompt('Sad', ITK)
